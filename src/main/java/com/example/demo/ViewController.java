00cc28548bc3fca24c854cff2591d7ece98f4d53
shotaro-0131 <shotaro8991@icloud.com>
2020-08-19 13:40:33 +0000

    create mysite

---
+++ src/main/java/com/example/demo/ViewController.java
@@ -0,0 +1,20 @@
+package com.example.demo;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
+public class ViewController {
+
+    @RequestMapping(value="/", method = RequestMethod.GET)
+    public ModelAndView view() {
+        ModelAndView modelAndView = new ModelAndView();
+        modelAndView.addObject("name", "村田　翔太朗");
+        modelAndView.addObject("birthday", "1月31日");
+        modelAndView.addObject("hobby", "麻雀");
+        modelAndView.setViewName("view");
+        return modelAndView;
+    }
+}
